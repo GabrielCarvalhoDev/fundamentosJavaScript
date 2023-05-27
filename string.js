d02e31bbd972caaf6bab231e536eab0cf7cdf168
Gabriel Carvalho <gabrielspere@gmail.com>
2023-05-27 16:04:47 -0300

    exemplos da Aula 1 e 2

---
+++ string.js
@@ -0,0 +1,8 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura12345!";
+const stringDeNumeros = "123456";
+const citacao = "Meu nome é ";
+const meuNome = "Gabriel";
+
+console.log(citacao + meuNome)
---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+const usuarioLogado = true;
+const contaPago = false;
+
+console.log( 0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+let minhaVar;
+let  varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
